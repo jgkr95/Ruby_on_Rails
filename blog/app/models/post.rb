52d0c896c23797636b09a7fbe37e7b12ddaa3d09
jgkr95 <jgkr95@msitprogram.net>
2020-10-31 10:10:48 +0530

    multile files upload

--- blog/app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post
-  include Mongoid::Document
-  field :Title, type: String
-  field :Genre, type: String
-  field :Description, type: String
-  field :Number_of_pages, type: Integer
-end
--- blog/app/views/posts/_post.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! post, :id, :Title, :Genre, :Description, :Number_of_pages, :created_at, :updated_at
-json.url post_url(post, format: :json)
